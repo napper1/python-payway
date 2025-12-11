dee2b78471379533521cf128eaa3b111e6711245
Ben Napper <ben@enrolhq.com.au>
2025-12-11 11:34:36 +1100

    Use dataclasses

--- payway/conf.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-PAYWAY_API_URL = "https://api.payway.com.au/rest/v1"
-TOKEN_URL = PAYWAY_API_URL + "/single-use-tokens-redirect"
-TRANSACTION_URL = PAYWAY_API_URL + "/transactions"
-CUSTOMER_URL = PAYWAY_API_URL + "/customers"
-OWN_BANK_ACCOUNTS_URL = PAYWAY_API_URL + "/your-bank-accounts"
-TOKEN_NO_REDIRECT = PAYWAY_API_URL + "/single-use-tokens"
